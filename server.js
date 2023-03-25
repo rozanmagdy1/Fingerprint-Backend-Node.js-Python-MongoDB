a78dbb5d4762863201736bccbaee6c9157794eda
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-03-25 23:58:29 +0200

    second

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use("/admin", AdminMiddleWare, adminApp);
 app.use("/website", UserMiddleWare,userApp);
 
 //const port = 8080;
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.listen(port, function (){
     console.log(`running on http://localhost:${port}`)
 });
