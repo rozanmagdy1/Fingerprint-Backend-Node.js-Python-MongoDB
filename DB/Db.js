e193a6978583941baca194811797e9ecc9e879d9
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-04-18 19:13:05 +0200

    alpha

--- DB/Db.js
+++ DB/Db.js
@@ -5,7 +5,7 @@ const MongoClient = mongodb.MongoClient;
 let url = process.env.MONGO_URI;
 function mongoConnect(url) {
     return new Promise((resolve, reject)=>{
-        MongoClient.connect(url, function (error, result) {
+        MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true },function (error, result) {
             if(error){reject(error)}
             resolve(result);
         })
