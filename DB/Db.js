a709d3b1d9666a72293f71bd66df567452fb419d
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-04-19 04:32:01 +0200

    alpha

--- DB/Db.js
+++ DB/Db.js
@@ -1,11 +1,11 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 //const url = "mongodb://localhost:27017";
-// const url = 'mongodb+srv://rozan:cluster0.rozan@cluster0.dqpcacg.mongodb.net/?retryWrites=true&w=majority';
-let url = process.env.MONGO_URI;
+const url = 'mongodb+srv://rozan:cluster0.rozan@cluster0.dqpcacg.mongodb.net/?retryWrites=true&w=majority';
+//let url = process.env.MONGO_URI;
 function mongoConnect(url) {
     return new Promise((resolve, reject)=>{
-        MongoClient.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true },function (error, result) {
+        MongoClient.connect(url,function (error, result) {
             if(error){reject(error)}
             resolve(result);
         })
