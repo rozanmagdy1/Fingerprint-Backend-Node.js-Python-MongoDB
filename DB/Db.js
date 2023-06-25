26e8f3ae50665f2ebfb6e32e4748488687e60415
rozanmagdy1 <rozanmagdy1@gmail.com>
2023-06-25 15:39:31 +0300

    alpha

--- DB/Db.js
+++ DB/Db.js
@@ -1,7 +1,7 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-// const url = 'mongodb+srv://rozan:cluster0.rozan@cluster0.dqpcacg.mongodb.net/?retryWrites=true&w=majority';
-let url = process.env.MONGO_URI
+const url = 'mongodb://localhost:27017';
+// let url = process.env.MONGO_URI
 function mongoConnect(url) {
     return new Promise((resolve, reject)=>{
         MongoClient.connect(url, function (error, result) {
